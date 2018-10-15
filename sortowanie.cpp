79e77771036edc902ab506548d4069b31aecc3e9
QSuHack <41905205+QSuHack@users.noreply.github.com>
2018-10-15 10:27:13 +0200

    gotowe sortowanie

---
+++ sortowanie.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <stdlib.h>
+using namespace std;
+int t [20];
+int main()
+{srand((unsigned)time(0));
+for (int i = 0; i <20; i++){
+    t[i] = rand() % 100+1;
+    cout << t[i] << " ";
+}
+cout <<endl;
+    for (int i=0; i<20; i++){
+        for (int j=0; j<20; j++){
+            if (t[j]>t[j+1])
+                swap(t[j], t[j+1]);
+        }}
+for (int i = 0 ; i <20; i++){
+    cout <<t[i]<<" " ;
+}
+
+getch();
+    return 0;
+}
