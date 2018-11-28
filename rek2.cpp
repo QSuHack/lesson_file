74ceb74dfd2ab98046fa5a5061ef5ed41c093d34
QSuHack <41905205+QSuHack@users.noreply.github.com>
2018-11-28 22:18:15 +0100

    Update rek2.cpp
    Repair -> Add "else"

--- rek2.cpp
+++ rek2.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-// wiesza sie
 using namespace std;
 HANDLE kon = GetStdHandle(STD_OUTPUT_HANDLE);
 
-void korale(int n){
+void korale(int n)
+{
 
-if (n==1 ){
+	if (n == 1)
+	{
 
-    SetConsoleTextAttribute(kon,3);
-    cout << '*';
-}
-if (n%2==0){
-    korale(n/2);
-    cout<< "o";
-}
-else{
-    korale((n-1)/2);
-    cout << '*';
-}
+		SetConsoleTextAttribute(kon, 3);
+		cout << '*';
+	}
+	else
+	{
+		if (n % 2 == 0)
+		{
+			korale(n / 2);
+			cout << "o";
+		}
+		else
+		{
+			korale((n - 1) / 2);
+			cout << '*';
+		}
+	}
 }
 
 int main()
 {
-    int a;
-cin >> a;
-korale(a);
-getch();
-    return 0;
+	int a;
+	cin >> a;
+	korale(a);
+	getch();
+	return 0;
 }
