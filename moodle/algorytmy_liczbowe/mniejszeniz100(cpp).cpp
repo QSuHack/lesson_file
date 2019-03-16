311694dc966eeb716f7983b32eaee7140f334084
QSuHack <41905205+QSuHack@users.noreply.github.com>
2019-03-16 20:16:26 +0100

    Add files via upload

---
+++ moodle/algorytmy_liczbowe/mniejszeniz100(cpp).cpp
@@ -0,0 +1,45 @@
+// mniejszeniz100(cpp).cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	int x;
+	int licznik = 0;
+	int o = 0;
+	int po = 0;
+	ifstream plik("liczby3.txt");
+	while (po != 0)
+	{
+		plik >> x;
+		if (x< 1000)
+		{
+			po = x;
+		}
+	}
+	while (o!=0){
+		plik >> x;
+		if( x< 1000){
+			o = x;
+		}
+	}
+
+	while (!plik.eof())
+	{
+		plik >> x;
+		if (x <1000){
+			po = o;
+			o = x;
+			licznik++;
+		}
+		
+	}
+	cout << licznik << " " << po << " " << o; 
+	return 0;
+
+}
