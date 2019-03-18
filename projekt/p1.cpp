45e7363abcc744917ed933b4abb6ee54ab9c6d51
QSuHack <41905205+QSuHack@users.noreply.github.com>
2019-03-18 10:36:41 +0100

    Add files via upload

---
+++ projekt/p1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+struct osoba {
+string PESEL;
+string imie;
+string nazwisko;
+string miasto;
+};
+void pobierz_dane(vector <osoba> &baza){
+
+    ifstream plik("baza.txt");
+    osoba c;
+    while(!plik.eof()){
+        plik >> c.imie >> c.nazwisko >> c.PESEL >> c.miasto;
+        baza.push_back(c);
+    }
+
+}
+time_t obecny_czas(){
+SYSTEMTIME st;
+GetLocalTime(&st);
+y=st.wYear;
+}
+int main()
+{
+
+    vector <osoba> baza;
+    pobierz_dane(baza);
+    for(auto x:baza){
+        cout << x.PESEL <<"\n";
+    }
+
+
+getch();
+    return 0;
+}
